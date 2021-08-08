8594fcfa1d7b22a6543b51c83d513c4a538b4fa6
David Freitas <davidfrei7as@outlook.com>
2021-08-08 12:19:00 -0300

    Template Site

--- views-cache/index.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/index.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,1 +1,452 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>Olá!</h1>
+<?php if(!class_exists('Rain\Tpl')){exit;}?>  <!--================Home Banner Area =================-->
+  <section class="home_banner_area mb-40">
+    <div class="banner_inner d-flex align-items-center">
+      <div class="container">
+        <div class="banner_content row">
+          <div class="col-lg-12">
+            <p class="sub text-uppercase">Your Setup Here</p>
+            <h3><span>Get</span> Your <br />Professional <span>Setup</span></h3>
+            <h4>Hightech products above together place.</h4>
+            <a class="main_btn mt-40" href="#">View More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--================End Home Banner Area =================-->
+
+  <!-- Start feature Area -->
+  <section class="feature-area section_gap_bottom_custom">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-6">
+          <div class="single-feature">
+            <a href="#" class="title">
+              <i class="flaticon-money"></i>
+              <h3>Money back gurantee</h3>
+            </a>
+            <p>Shall open divide a one</p>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6">
+          <div class="single-feature">
+            <a href="#" class="title">
+              <i class="flaticon-truck"></i>
+              <h3>Free Delivery</h3>
+            </a>
+            <p>Shall open divide a one</p>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6">
+          <div class="single-feature">
+            <a href="#" class="title">
+              <i class="flaticon-support"></i>
+              <h3>Alway support</h3>
+            </a>
+            <p>Shall open divide a one</p>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6">
+          <div class="single-feature">
+            <a href="#" class="title">
+              <i class="flaticon-blockchain"></i>
+              <h3>Secure payment</h3>
+            </a>
+            <p>Shall open divide a one</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End feature Area -->
+
+  <!--================ Feature Product Area =================-->
+  <section class="feature_product_area section_gap_bottom_custom">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-12">
+          <div class="main_title">
+            <h2><span>Featured product</span></h2>
+            <p>Bring called seed first of third give itself now ment</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-3 col-md-6 col-6">
+          <a href="#">
+            <div class="single-product">
+              <div class="product-img">
+                <img class="img-fluid w-100" src="/res/site/img/product/ssd-nvme.png" alt="" />
+              </div>
+              <div class="product-btm">
+                <a href="#" class="d-block">
+                  <h4>Latest products</h4>
+                </a>
+                <div class="mt-3">
+                  <span class="mr-4">$25.00</span>
+                  <del>$35.00</del>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-6">
+          <a href="#">
+            <div class="single-product">
+              <div class="product-img">
+                <img class="img-fluid w-100" src="/res/site/img/product/intel.png" alt="" />
+              </div>
+              <div class="product-btm">
+                <a href="#" class="d-block">
+                  <h4>Latest products</h4>
+                </a>
+                <div class="mt-3">
+                  <span class="mr-4">$25.00</span>
+                  <del>$35.00</del>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-6">
+          <a href="#">
+            <div class="single-product">
+              <div class="product-img">
+                <img class="img-fluid w-100" src="/res/site/img/product/placa-mae.png" alt="" />
+              </div>
+              <div class="product-btm">
+                <a href="#" class="d-block">
+                  <h4>Latest products</h4>
+                </a>
+                <div class="mt-3">
+                  <span class="mr-4">$25.00</span>
+                  <del>$35.00</del>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-6">
+          <a href="#">
+            <div class="single-product">
+              <div class="product-img">
+                <img class="img-fluid w-100" src="/res/site/img/product/kitgamer.png" alt="" />
+              </div>
+              <div class="product-btm">
+                <a href="#" class="d-block">
+                  <h4>Latest products</h4>
+                </a>
+                <div class="mt-3">
+                  <span class="mr-4">$25.00</span>
+                  <del>$35.00</del>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--================ End Feature Product Area =================-->
+
+  <!--================ Offer Area =================-->
+  <section class="offer_area">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="offset-lg-4 col-lg-6 text-center">
+          <div class="offer_content">
+            <h3 class="text-uppercase mb-40">Notebook Accessories</h3>
+            <h2 class="text-uppercase">50% off</h2>
+            <a href="#" class="main_btn mb-20 mt-5">Discover Now</a>
+            <p>Limited Time Offer</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--================ End Offer Area =================-->
+
+  <!--================ New Product Area =================-->
+  <section class="new_product_area section_gap_top section_gap_bottom_custom">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-12">
+          <div class="main_title">
+            <h2><span>new products</span></h2>
+            <p>Bring called seed first of third give itself now ment</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="new_product">
+            <h5 class="text-uppercase">collection of 2021</h5>
+            <h3 class="text-uppercase">Best Accessories</h3>
+            <div class="product-img">
+              <img class="img-fluid" src="/res/site/img/product/kitgamer-banner.png" alt="" />
+            </div>
+            <h4>$120.70</h4>
+            <a href="#" class="main_btn">Add to cart</a>
+          </div>
+        </div>
+
+        <div class="col-lg-6 mt-5 mt-lg-0">
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-6">
+              <a href="#">
+                <div class="single-product">
+                  <div class="product-img">
+                    <img class="img-fluid w-100" src="/res/site/img/product/memoria.png" alt="" />
+                  </div>
+                  <div class="product-btm">
+                    <a href="#" class="d-block">
+                      <h4>Latest products</h4>
+                    </a>
+                    <div class="mt-3">
+                      <span class="mr-4">$25.00</span>
+                      <del>$35.00</del>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-6">
+              <a href="#">
+                <div class="single-product">
+                  <div class="product-img">
+                    <img class="img-fluid w-100" src="/res/site/img/product/gabinete.png" alt="" />
+                  </div>
+                  <div class="product-btm">
+                    <a href="#" class="d-block">
+                      <h4>Latest products</h4>
+                    </a>
+                    <div class="mt-3">
+                      <span class="mr-4">$25.00</span>
+                      <del>$35.00</del>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-6">
+              <a href="#">
+                <div class="single-product">
+                  <div class="product-img">
+                    <img class="img-fluid w-100" src="/res/site/img/product/intel.png" alt="" />
+                  </div>
+                  <div class="product-btm">
+                    <a href="#" class="d-block">
+                      <h4>Latest products</h4>
+                    </a>
+                    <div class="mt-3">
+                      <span class="mr-4">$25.00</span>
+                      <del>$35.00</del>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+
+            <div class="col-lg-6 col-md-6 col-6">
+              <a href="#">
+                <div class="single-product">
+                  <div class="product-img">
+                    <img class="img-fluid w-100" src="/res/site/img/product/amd.png" alt="" />
+                  </div>
+                  <div class="product-btm">
+                    <a href="#" class="d-block">
+                      <h4>Latest products</h4>
+                    </a>
+                    <div class="mt-3">
+                      <span class="mr-4">$25.00</span>
+                      <del>$35.00</del>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--================ End New Product Area =================-->
+
+  <!--================ Inspired Product Area =================-->
+  <section class="inspired_product_area section_gap_bottom_custom">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-12">
+          <div class="main_title">
+            <h2><span>Inspired products</span></h2>
+            <p>Bring called seed first of third give itself now ment</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-3 col-md-6 col-6">
+          <a href="#">
+            <div class="single-product">
+              <div class="product-img">
+                <img class="img-fluid w-100" src="/res/site/img/product/memoria.png" alt="" />
+              </div>
+              <div class="product-btm">
+                <a href="#" class="d-block">
+                  <h4>Latest products</h4>
+                </a>
+                <div class="mt-3">
+                  <span class="mr-4">$25.00</span>
+                  <del>$35.00</del>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-6">
+          <a href="#">
+            <div class="single-product">
+              <div class="product-img">
+                <img class="img-fluid w-100" src="/res/site/img/product/placa-mae.png" alt="" />
+              </div>
+              <div class="product-btm">
+                <a href="#" class="d-block">
+                  <h4>Latest products</h4>
+                </a>
+                <div class="mt-3">
+                  <span class="mr-4">$25.00</span>
+                  <del>$35.00</del>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-6">
+          <a href="#">
+            <div class="single-product">
+              <div class="product-img">
+                <img class="img-fluid w-100" src="/res/site/img/product/amd.png" alt="" />
+              </div>
+              <div class="product-btm">
+                <a href="#" class="d-block">
+                  <h4>Latest products</h4>
+                </a>
+                <div class="mt-3">
+                  <span class="mr-4">$25.00</span>
+                  <del>$35.00</del>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-6">
+          <a href="#">
+            <div class="single-product">
+              <div class="product-img">
+                <img class="img-fluid w-100" src="/res/site/img/product/intel.png" alt="" />
+              </div>
+              <div class="product-btm">
+                <a href="#" class="d-block">
+                  <h4>Latest products</h4>
+                </a>
+                <div class="mt-3">
+                  <span class="mr-4">$25.00</span>
+                  <del>$35.00</del>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        
+        <div class="col-lg-3 col-md-6 col-6">
+          <a href="#">
+            <div class="single-product">
+              <div class="product-img">
+                <img class="img-fluid w-100" src="/res/site/img/product/kitgamer.png" alt="" />
+              </div>
+              <div class="product-btm">
+                <a href="#" class="d-block">
+                  <h4>Latest products</h4>
+                </a>
+                <div class="mt-3">
+                  <span class="mr-4">$25.00</span>
+                  <del>$35.00</del>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-6">
+          <a href="#">
+            <div class="single-product">
+              <div class="product-img">
+                <img class="img-fluid w-100" src="/res/site/img/product/ssd-nvme.png" alt="" />
+              </div>
+              <div class="product-btm">
+                <a href="#" class="d-block">
+                  <h4>Latest products</h4>
+                </a>
+                <div class="mt-3">
+                  <span class="mr-4">$25.00</span>
+                  <del>$35.00</del>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-6">
+          <a href="#">
+            <div class="single-product">
+              <div class="product-img">
+                <img class="img-fluid w-100" src="/res/site/img/product/placa-mae.png" alt="" />
+              </div>
+              <div class="product-btm">
+                <a href="#" class="d-block">
+                  <h4>Latest products</h4>
+                </a>
+                <div class="mt-3">
+                  <span class="mr-4">$25.00</span>
+                  <del>$35.00</del>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-6">
+          <a href="#">
+            <div class="single-product">
+              <div class="product-img">
+                <img class="img-fluid w-100" src="/res/site/img/product/memoria.png" alt="" />
+              </div>
+              <div class="product-btm">
+                <a href="#" class="d-block">
+                  <h4>Latest products</h4>
+                </a>
+                <div class="mt-3">
+                  <span class="mr-4">$25.00</span>
+                  <del>$35.00</del>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--================ End Inspired Product Area =================-->
